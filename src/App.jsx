3f0cd791e449bb5ebcde6fd6813ce8e13d0abe5b
jose <jmiguelsaraiva2727@gmail.com>
2025-03-21 23:59:07 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -7,31 +6,18 @@ import Login from "./pages/Login";
 import Destaques from "./pages/Destaques";
 import Logout from "./pages/Logout";
 import Profile from "./pages/Profile";
-import Wrapper from "./pages/Wrapper";
-
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Home: Página principal  */}
         <Route path="/" element={<Home />} />
-
-        {/* Registo */}
         <Route path="/register" element={<Register />} />
-
-        {/* Login */}
         <Route path="/login" element={<Login />} />
-
-        {/* Destaques */}
         <Route path="/destaques" element={<Destaques />} />
-
-        {/* Logout protegido: só acessível a utilizadores autenticados */}
-        <Route path="/logout" element={<Wrapper> <Logout /> </Wrapper>} />
-
-        {/* Profile protegido */}
-        <Route path="/profile" element={ <Wrapper> <Profile /> </Wrapper>} />
-
+        <Route path="/logout" element={<Wrapper><Logout /></Wrapper>} />
+        <Route path="/profile" element={<Wrapper><Profile /></Wrapper>} />
       </Routes>
     </BrowserRouter>
   );
